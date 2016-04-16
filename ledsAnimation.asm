f0f40a4c15329a33404c973f23e3241caa6ec8bb
Augusto Hoffmann <hoffmann.augusto@gmail.com>
2016-04-16 19:05:59 -0300

    v1.1
    Retirado auxReg2

--- ledsAnimation.asm
+++ ledsAnimation.asm
@@ -21,7 +21,6 @@
 ; Register and constants definitions
 ; ------------------------------------------------------------------------------
 .def		auxReg		= R16
-.def		auxReg2		= R17
 ;.def		counter		= R18
 .def		pushedLed	= R19
 .def		pusherLeds	= R20
@@ -89,8 +88,8 @@ endCompare:
 ; Dependencies:	-
 ; ------------------------------------------------------------------------------
 pusherMove:
-	MOV		auxReg2, pusherLeds			; Uses auxReg2 to compare pushedLed
-	AND		auxReg2, pushedLed			;	with pusherLeds
+	MOV		auxReg, pusherLeds			; Uses auxReg2 to compare pushedLed
+	AND		auxReg, pushedLed			;	with pusherLeds
 	BRNE	endPusherMove				; If != 0 MSB of pusherLeds = pushedLed
 	CALL	showLeds					; If 0, show leds,
 	LSL		pusherLeds					; Shift left pusherLeds
